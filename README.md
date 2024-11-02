28b470060bac9f895fc5e0d12198a5c5d080dc54
shakhbozbekusmonov <shakhbozbek.usmonov@gmail.com>
2024-11-02 10:51:06 +0500

    done: 1-day ✨

---
+++ README.md
@@ -0,0 +1,8 @@
+# 100 Days Of Python
+
+- 1-Day
+  - username length.
+  - swap number.
+  - band name generator.
+- 2-Day
+- 3-Day
---
+++ day-1/main.py
@@ -0,0 +1,27 @@
+
+# Task 1 -- Solution
+"""
+username = input("What is your name? >>> ")
+
+print(len(username))
+"""
+
+# Task 2 -- Solution
+"""
+a = input("a: ")
+b = input("b: ")
+
+a, b = b, a
+
+print("a =", a)
+print("b =", b)
+"""
+
+# Task 3 -- Solution
+print("Welcome to the Band Name Generator.")
+city = input("What's name of the city you grew up in? >>> ")
+print(city)
+pet = input("What's your pet's name? >>> ")
+print(pet)
+print("Your brand name could be " + city + " " + pet)
+
